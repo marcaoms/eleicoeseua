1341119e13df7ec98bc37242c30c949e0e257db6
Marcos Ferreira <101142422+marcaoms@users.noreply.github.com>
2024-09-08 11:57:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,14 @@
+let dados = [
+{
+    titulo: "Kamala Harris",
+    descricao: "Kamala Harris é uma política americana que atualmente serve como Vice-Presidente dos Estados Unidos, cargo que ocupa desde janeiro de 2021. Anteriormente, foi Senadora pela Califórnia e Procuradora-Geral do mesmo estado. Harris é conhecida por seu trabalho em justiça criminal e direitos civis, e sua eleição como a primeira mulher negra e sul-asiática na vice-presidência marca um marco significativo na política americana.",
+    link: "https://kamalaharris.com/meet-kamala-harris/",
+    tags: "democrata mulher"
+},
+{
+    titulo: "Donald Trump",
+    descricao: "Donald Trump é um empresário e político americano que serviu como o 45º Presidente dos Estados Unidos de janeiro de 2017 a janeiro de 2021. Antes de sua presidência, Trump era conhecido por seu trabalho como empresário e personalidade de mídia, incluindo o programa de televisão The Apprentice. Durante sua presidência, ele focou em políticas de imigração, economia e reforma tributária. Trump é uma figura polarizadora na política americana, com um forte apoio e oposição acirrada.",
+    link: "https://www.donaldjtrump.com/",
+    tags: "republicano homem"
+}
+];
